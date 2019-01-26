bc222c8563fe1ca13e19fcfd57c2ebe4b6fbdab0
sweshixing <249978022@qq.com>
2019-01-26 13:49:48 +0800

    添加admin的安全验证功能

--- admin-server/src/main/java/com/adminserver/adminserver/AdminServerApplication.java
+++ admin-server/src/main/java/com/adminserver/adminserver/AdminServerApplication.java
@@ -1,10 +1,15 @@
 package com.adminserver.adminserver;
 
+import de.codecentric.boot.admin.server.config.AdminServerProperties;
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
 
 @SpringBootApplication
 @Configuration
@@ -16,5 +21,45 @@ public class AdminServerApplication {
         SpringApplication.run(AdminServerApplication.class, args);
     }
 
+
+    @Profile("insecure")
+    @Configuration
+    public static class SecurityPermitAllConfig extends WebSecurityConfigurerAdapter {
+
+        @Override
+        protected void configure(HttpSecurity http) throws Exception{
+            http.authorizeRequests().anyRequest().permitAll()
+                    .and().csrf().disable();
+        }
+    }
+
+    @Profile("secure")
+    @Configuration
+    public static class SecuritySecureConfig extends WebSecurityConfigurerAdapter{
+        private final String adminContextPath;
+
+        public SecuritySecureConfig(AdminServerProperties adminServerProperties) {
+            this.adminContextPath = adminServerProperties.getContextPath();
+        }
+
+        @Override
+        protected void configure(HttpSecurity http) throws Exception{
+            SavedRequestAwareAuthenticationSuccessHandler awareAuthenticationSuccessHandler = new SavedRequestAwareAuthenticationSuccessHandler();
+            awareAuthenticationSuccessHandler.setTargetUrlParameter("redirectTo");
+
+            http.authorizeRequests()
+                    .antMatchers(adminContextPath+"/login").permitAll()
+                    .anyRequest().authenticated()
+                    .and()
+                    .formLogin().loginPage(adminContextPath+"/login").successHandler(awareAuthenticationSuccessHandler)
+                    .and()
+                    .logout().logoutUrl(adminContextPath+"/logout")
+                    .and()
+                    .httpBasic()
+                    .and()
+                    .csrf().disable();
+        }
+
+    }
 }
 
--- admin-server/src/main/java/com/adminserver/adminserver/security.java
+++
@@ -1,4 +0,0 @@
-package com.adminserver.adminserver;
-
-public class security {
-}
